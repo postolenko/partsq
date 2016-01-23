758c7e9bb2e6ff3325e97a9609f7017e6138eef8
postolenko <denis.postolenko@gmail.com>
2016-01-23 03:12:53 +0200

    PartdQ

--- category/categories.js
+++ category/categories.js
@@ -11,16 +11,32 @@ $(document).ready(function() {
 
 		categoryListHeight = $(".catlist:eq("+ indexCategory +")").height();
 
-		$(".arrowcat:eq("+ indexCategory +")").toggleClass("glyphicon-menu-up");
+		// $(".arrowcat:eq("+ indexCategory +")").toggleClass("glyphicon-menu-down");
 
 		if ( $(".catheight:eq("+ indexCategory +")").height() > 0 ) {
 
-			$(".catheight:eq("+ indexCategory +")").animate({"height": 0}, 400);
+			$(".catheight:eq("+ indexCategory +")").animate({"height": 0}, 400);			
+
+			if ($(".arrowcat:eq("+ indexCategory +")").hasClass("glyphicon-menu-up")) {
+
+				$(".arrowcat:eq("+ indexCategory +")").removeClass("glyphicon-menu-up");
+
+				$(".arrowcat:eq("+ indexCategory +")").addClass("glyphicon-menu-down");
+
+			}			
 
 		} else {
 
 			$(".catheight:eq("+ indexCategory +")").animate({"height": categoryListHeight + "px"}, 400);
 
+			if ($(".arrowcat:eq("+ indexCategory +")").hasClass("glyphicon-menu-down")) {
+
+				$(".arrowcat:eq("+ indexCategory +")").removeClass("glyphicon-menu-down");
+
+				$(".arrowcat:eq("+ indexCategory +")").addClass("glyphicon-menu-up");
+
+			}		
+		
 		}
 
 	});
