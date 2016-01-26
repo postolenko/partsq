b4363c1edfba7b312f6ad6d0812e3de4f3d689e2
postolenko <denis.postolenko@gmail.com>
2016-01-26 20:32:38 +0200

    PartsQ

--- js/scripts.js
+++ js/scripts.js
@@ -154,7 +154,7 @@ $(document).ready(function() {
 
 
     $(document).on("pageshow", "[data-role='page'].no-loadmsg", function () {
-        $('div.ui-loader').remove();
+        $("div .ui-loader").remove();
     });
 
 
